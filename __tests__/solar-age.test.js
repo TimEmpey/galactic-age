e11650b994e4906e281c863b2bf47335c8d18c5f
Tim Empey <timothyempey@gmail.com>
2022-09-02 15:32:28 -0700

    add alternate test for coverage

--- __tests__/solar-age.test.js
+++ __tests__/solar-age.test.js
@@ -62,6 +62,11 @@ describe('AgeInput', () => {
     expect(input.marsYears()).toEqual(122);
   });
 
+  test('should say how many years until user is 169 or how many years past 169 on Mars', () => {
+    let input = new AgeInput(25, 6, 16, 200);
+    expect(input.marsYears()).toEqual(31);
+  });
+
   test('should say how many years until user is 1067 or how many years past 1067 on Jupiter', () => {
     let input = new AgeInput(25, 6, 16, 47, 297);
     expect(input.jupiterYears()).toEqual(770);

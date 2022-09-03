fef4cef2c285cfb19a76d570c8908821caaa66f5
Tim Empey <timothyempey@gmail.com>
2022-09-03 11:09:58 -0700

    update configuration

--- webpack.config.js
+++ webpack.config.js
@@ -42,26 +42,6 @@ module.exports = {
   module: {
     rules: [
       {
-        loaders: [
-          {
-            test: /\.scss$/,
-            loader: 'style-loader!css-loader!sass-loader'
-          },
-          {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'fonts/'
-                }
-              }
-            ]
-          }
-        ]
-      },
-      {
         test: /\.(gif|png|avif|jpe?g)$/,
         use: [
           {

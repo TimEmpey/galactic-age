98b6622a46288e693ddcd157480ef12d37e6268f
Tim Empey <timothyempey@gmail.com>
2022-09-02 23:37:56 -0700

    add configuration for importing fonts

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,26 @@ module.exports = {
   module: {
     rules: [
       {
+        loaders: [
+          {
+            test: /\.scss$/,
+            loader: 'style-loader!css-loader!sass-loader'
+          },
+          {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
         test: /\.(gif|png|avif|jpe?g)$/,
         use: [
           {
@@ -59,7 +79,6 @@ module.exports = {
           'html-loader'
         ]
       },
-      
       {
         test: /\.css$/,
         use: [
